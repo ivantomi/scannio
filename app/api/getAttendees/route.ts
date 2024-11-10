31835a8acd65f12a94fee9acebe36304e7df7721
Ivan Tomić <45340642+ivantomi@users.noreply.github.com>
2024-11-10 21:07:11 +0100

    Update route.ts

--- app/api/getAttendees/route.ts
+++ app/api/getAttendees/route.ts
@@ -1,12 +1,17 @@
 import prisma from "@/app/lib/prisma";
+import { AttendeeWithEntries } from "@/app/lib/types";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const attendees = await prisma.attendee.findMany({
+  const attendees = (await prisma.attendee.findMany({
     include: {
-      entries: true,
+      entries: {
+        orderBy: {
+          day: "asc",
+        },
+      },
     },
-  });
+  })) as AttendeeWithEntries[];
 
   console.log(attendees);
 
